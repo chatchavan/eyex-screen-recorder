d61de35f4b23bfcfb1236fb0da0aaa1a2881065c
Chat Wacharamanotham <chat@ifi.uzh.ch>
2016-06-17 05:04:25 +0200

    CSV logging + visualizing last 10 case points in the video

--- screencapture/screencapture/VideoRecorder.cs
+++ screencapture/screencapture/VideoRecorder.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using EyeXFramework;
 using Tobii.EyeX.Framework;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
 
 namespace screencapture
 {
@@ -18,8 +21,16 @@ namespace screencapture
         private bool isRecording;
         private bool writing = false;
         double gazeX, gazeY;
+        const int prevGazeQueueSize = 10;
+        Queue<double> prevGazeXQ = new Queue<double>(prevGazeQueueSize);
+        Queue<double> prevGazeYQ = new Queue<double>(prevGazeQueueSize);
+        
         EyeXHost _eyeXHost;
         GazePointDataStream _lightlyFilteredGazeDataStream;
+
+        private TextWriter textWriter;
+        private CsvWriter csv;
+        private CultureInfo usCulture = new CultureInfo("en-US"); 
 
         private Size screenSize;
 
@@ -35,7 +46,18 @@ namespace screencapture
 
         public void StartRecording(String filenameToSave){
             videoWriter = new VideoFileWriter();
-            videoWriter.Open(filenameToSave, screenSize.Width, screenSize.Height, 25, VideoCodec.MPEG2);
+            videoWriter.Open(filenameToSave, screenSize.Width, screenSize.Height, 25, VideoCodec.MPEG4, 8000000);
+
+            // init csv
+            textWriter = File.CreateText(Path.ChangeExtension(filenameToSave, ".csv"));
+            csv = new CsvWriter(textWriter);
+            csv.Configuration.CultureInfo = usCulture;
+            csv.WriteField("Timestamp");
+            csv.WriteField("GazeX");
+            csv.WriteField("GazeY");
+            csv.NextRecord();
+
+
             frameTimer.Start();
             isRecording = true;
             StartEyeStream();
@@ -46,6 +68,7 @@ namespace screencapture
             frameTimer.Stop();
             while (writing) { }     // prevent closing in the middle of writing
             videoWriter.Close();
+            textWriter.Close();
             StopEyeStream();
         }
 
@@ -64,6 +87,20 @@ namespace screencapture
         {
             gazeX = e.X;
             gazeY = e.Y;
+            
+            // record CSV
+            csv.WriteField(e.Timestamp);
+            csv.WriteField(gazeX);
+            csv.WriteField(gazeY);
+            csv.NextRecord();
+
+            // add to queue
+            prevGazeXQ.Enqueue(gazeX);
+            prevGazeYQ.Enqueue(gazeY);
+            if (prevGazeXQ.Count >= prevGazeQueueSize) {
+                prevGazeXQ.Dequeue();
+                prevGazeYQ.Dequeue();
+            }
         }
 
         private void StopEyeStream()
@@ -81,9 +118,30 @@ namespace screencapture
                 using (Graphics g = Graphics.FromImage(frameImage))
                 {
                     g.CopyFromScreen(0, 0, 0, 0, screenSize, CopyPixelOperation.SourceCopy);
-                    Color red = Color.FromArgb(0x60, 0xff, 0, 0);
+                    
+                    int ellipseSize = 10;
+                    
+                    // previous gaze points
+                    double[] prevGazeXs = new double[prevGazeXQ.Count];
+                    double[] prevGazeYs = new double[prevGazeYQ.Count];
+                    prevGazeXQ.CopyTo(prevGazeXs, 0);
+                    prevGazeYQ.CopyTo(prevGazeYs, 0);
+                    Point[] prevGazePoints = new Point[prevGazeXQ.Count];
+                    for (int i = 0; i < prevGazeXQ.Count; i++) {
+                        Brush prevRedBrush = new SolidBrush(Color.FromArgb((i * 245/prevGazeQueueSize) +10, 0xff, 0, 0));
+                        g.FillEllipse(prevRedBrush, (int) prevGazeXs[i] - (ellipseSize / 2), (int) prevGazeYs[i] - (ellipseSize / 2), ellipseSize, ellipseSize);
+                        prevGazePoints[i] = new Point((int) prevGazeXs[i], (int) prevGazeYs[i]);
+                    }
+                    if (prevGazeXQ.Count > 2) {
+                        Pen gazeLinePen = new Pen(new SolidBrush(Color.FromArgb(180, 0xff, 0, 0)));
+                        g.DrawLines(gazeLinePen, prevGazePoints);
+                    }
+                    
+
+                    // latest gaze point
+                    Color red = Color.FromArgb(255, 0xff, 0, 0);
                     Brush redBrush = new SolidBrush(red);
-                    g.FillEllipse(redBrush, (int)gazeX-50, (int)gazeY-50, 100, 100);
+                    g.FillEllipse(redBrush, (int) gazeX - (ellipseSize / 2), (int) gazeY - (ellipseSize / 2), ellipseSize, ellipseSize);
                 }
                 if (isRecording)
                 {
